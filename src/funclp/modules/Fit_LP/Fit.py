a953c8c37b8697c7750d3ecb11887f3ccd6eec0d
LancelotPincet <lancelot.pincet@free.fr>
2026-01-28 18:36:01 +0100

    auto-commit

--- src/funclp/modules/Fit_LP/Fit.py
+++ src/funclp/modules/Fit_LP/Fit.py
@@ -17,6 +17,7 @@ from corelp import prop, selfkwargs
 from funclp import CudaReference, use_inputs, use_shapes, use_cuda, use_broadcasting
 from abc import ABC, abstractmethod
 import numpy as np
+import numba as nb
 
 
 
@@ -52,34 +53,45 @@ class Fit(ABC, CudaReference) :
         self.estimator.cuda_reference, self.estimator_cuda_rederence = self.function, self.estimator.cuda_reference
         selfkwargs(self, kwargs)
 
-    # Attributs
-    max_iterations = 200 #Maximum number of iterations
-
-    # Parameters
+    # Function and estimator
     @property
     def function(self) :
         return self._function
     @property
     def estimator(self) :
         return self._estimator
-    @prop(link='function')
-    def parameters(self) :
-        return 'parameters'
+
+    # Attributs
+    max_iterations = 200 #Maximum number of iterations
+    max_retries = 10 # Maximum of step retries for a given step change
+    ftol = 1e-6 # Loop stops when chi2 change is lower than ftol.
+    xtol = 0 # Loop stops when parameter step is lower than xtol.
+    gtol = 0 # Loop stops when gradient maximum is lower than gtol.
+
+    # Parameters to fit
     @property
     def parameters2fit(self) :
-        return [key for key in self.parameters if getattr(self.function, f'{key}_fit')]
+        return [key for key in self.function.parameters.keys() if getattr(self.function, f'{key}_fit')]
     @property
     def nparameters2fit(self) :
         return len(self.parameters2fit)
+
+
 
     # Apply model
     def model(self) :
         self.function(*self.variables, *self.data, **self.parameters, out=self.model_data, ignore=self.converged)
 
+
+
     #ABC
     @abstractmethod
-    def fit(self) :
-        ''' Method defining the core algorithm behind the fitting method '''
+    def fit_init(self) :
+        ''' Fit initialization specific to optimizer '''
+        pass
+    @abstractmethod
+    def fit_optimize(self) :
+        ''' Fit main logic optimizer '''
         pass
     def __call__(self, raw_data, *args, weights=np.float32(1.)) :
         ''' Fitting function '''
@@ -90,20 +102,304 @@ class Fit(ABC, CudaReference) :
         (nomodel, nopoint), (self.nmodels, self.npoints), in_shapes = use_shapes(*inputs) # (nomodel, nopoint), (nmodels, npoints), (variables_shapes, data_shapes, parameters_shapes)
         self.cuda, self.xp, transfer_back, blocks_per_grid, threads_per_block = use_cuda(self.function, (self.nmodels, self.npoints), inputs)
         self.variables, self.data, self.parameters, self.dtype = use_broadcasting(self.xp, *inputs, *in_shapes, (self.nmodels, self.npoints))
+
+        # Allocate memory
         self.raw_data = self.xp.asarray(raw_data).reshape((self.nmodels, self.npoints))
         self.model_data = self.xp.empty(shape=(self.nmodels, self.npoints), dtype=self.dtype)
         self.weights = self.xp.asarray(weights)
+        self.parameter_steps = self.xp.empty(shape=(self.nmodels, self.nparameters2fit, 1), dtype=self.dtype)
         self.converged = self.xp.zeros(shape=self.nmodels, dtype=self.xp.bool_)
+        self.jacobian_data = self.xp.empty(shape=(self.nmodels, self.npoints, self.nparameters2fit), dtype=self.dtype)
+        self.deviance_data = self.xp.empty(shape=(self.nmodels, self.npoints), dtype=self.dtype)
+        self.chi2_data = self.xp.empty(shape=self.nmodels, dtype=self.dtype)
+        self.chi2_cache = self.xp.empty_like(self.chi2_data)
+        self.loss_data = self.xp.empty(shape=(self.nmodels, self.npoints), dtype=self.dtype)
+        self.gradient_data = self.xp.empty(shape=(self.nmodels, self.nparameters2fit, 1), dtype=self.dtype)
+        self.fisher_data = self.xp.empty(shape=(self.nmodels, self.npoints), dtype=self.dtype)
+        self.hessian_data = self.xp.empty(shape=(self.nmodels, self.nparameters2fit, self.nparameters2fit), dtype=self.dtype)
 
-        # Algorithm fit
-        self.fit()
+        # Initialize cached chi2
+        self.model()
+        self.chi2()
+        self.chi2_cache[:] = self.chi2_data
+
+        # Iterations
+        for _ in range(self.max_iterations) :
+
+            # Evaluate local model
+            self.jacobian()
+            self.gradient()
+            self.hessian()
+
+            # Fit main logic [depends on optimizer]
+            self.fit_optimize()
+
+            # Calculate convergence
+            self.convergence()
+            if self.converged.all() :
+                break
 
         # End
-        if transfer_back : self.parameters = {key: self.xp.asnumpy(value) for key, value in self.parameters.items()}
-        if nomodel : self.parameters = {key: value.item() for key, value in self.parameters.items()}
-        self.function.parameters = self.parameters
+        if transfer_back : self.parameters = [self.xp.asnumpy(param) for param in self.parameters]
+        if nomodel : self.parameters = [param.item() for param in self.parameters]
         self.cuda = cache_cuda
-        return self.parameters
+        self.function.parameters = {key: self.parameters[pos] if getattr(self.function, f'{key}_fit') else self.function.parameters[key] for pos, key in enumerate(self.function.parameters.keys())}
+        return {key: self.function.parameters[key] for key in self.parameters2fit}
+
+
+
+    # --- Chi² ---
+
+    def chi2(self) :
+        self.estimator.deviance(self.raw_data, self.model_data, weights=self.weights, out=self.deviance_data, ignore=self.converged)
+        deviance2chi2_function = self.gpu_deviance2chi2 if self.cuda else self.cpu_deviance2chi2
+        deviance2chi2_function(self.deviance_data, self.chi2_data, self.converged)
+
+    @prop(cache=True)
+    def cpu_deviance2chi2(self) :
+        @nb.njit(parallel=True)
+        def func(deviance, chi2, converged) :
+            nmodels, npoints = deviance.shape
+            for model in nb.prange(nmodels) :
+                if converged[model] : continue
+                s = 0.0
+                for point in range(npoints) :
+                    s += deviance[model, point]
+                chi2[model] = s
+        return func
+
+    @prop(cache=True)
+    def gpu_deviance2chi2(self) :
+        @nb.cuda.jit()
+        def func(deviance, chi2, converged) :
+            nmodels, npoints = deviance.shape
+            model = nb.cuda.grid(1)  # 1D grid of threads
+            if model < nmodels and not converged[model] :
+                s = 0.0
+                for point in range(npoints):
+                    s += deviance[model, point]
+                chi2[model] = s
+        threads_per_block = 128
+        blocks_per_grid = (self.nmodels + threads_per_block - 1) // threads_per_block
+        return func[blocks_per_grid, threads_per_block]
+
+
+
+    # --- Jacobian ---
+
+    def jacobian(self) :
+        jacobian_function = self.gpu_jacobian if self.cuda else self.cpu_jacobian
+        jacobian_function(*self.variables, *self.data, *self.parameters.values(), self.jacobian_data, self.converged)
+
+    @prop(cache=True)
+    def cpu_jacobian(self) :
+        kernels = {f"d_{parameter}": getattr(self.function, f'cpukernel_d_{parameter}') for parameter in self.parameters2fit}
+        inputs = ''
+        inputs += ', '.join(self.function.variables) + ', ' if len(self.function.variables) > 0 else ''
+        inputs += ', '.join(self.function.data) + ', ' if len(self.function.data) > 0 else ''
+        inputs += ', '.join(self.parameters.keys())
+        string = f'''
+    @nb.njit(parallel=True)
+    def func({inputs}, jacobian, ignore) :
+    nmodels, npoints, nparams = jacobian.shape
+    for model in nb.prange(nmodels) :
+        if ignore[model] : continue
+        for point in range(npoints) :
+            for param in range(nparams) :
+    '''
+        for param, parameter in enumerate(self.parameters2fit) :
+            string += f'''
+                if param == {param} :
+                    jacobian[model, point, param] = d_{parameter}({inputs})
+    '''
+        glob = {'nb': nb}
+        glob.update(kernels)
+        loc = {}
+        exec(string, glob, loc)
+        func = loc['func']
+        return func
+
+    @prop(cache=True)
+    def gpu_jacobian(self) :
+        kernels = {f"d_{parameter}": getattr(self.function, f'cpukernel_d_{parameter}') for parameter in self.parameters2fit}
+        inputs = ''
+        inputs += ', '.join(self.function.variables) + ', ' if len(self.function.variables) > 0 else ''
+        inputs += ', '.join(self.function.data) + ', ' if len(self.function.data) > 0 else ''
+        inputs += ', '.join(self.parameters.keys())
+        string = f'''
+    @nb.cuda.jit(parallel=True)
+    def func({inputs}, jacobian, ignore) :
+    nmodels, npoints, nparams = jacobian.shape
+    model, point, param = nb.cuda.grid(3)
+    if model < nmodels and not ignore[model] and point < npoints and param < nparams :
+    '''
+        for param, parameter in enumerate(self.parameters2fit) :
+            string += f'''
+        if param == {param} :
+            jacobian[model, point, param] = d_{parameter}({inputs})
+    '''
+        glob = {'nb': nb}
+        glob.update(kernels)
+        loc = {}
+        exec(string, glob, loc)
+        func = loc['func']
+        threads_per_block = 8, 8, 8
+        blocks_per_grid = (
+            (self.nmodels + threads_per_block[0] - 1) // threads_per_block[0],
+            (self.npoints + threads_per_block[1] - 1) // threads_per_block[1],
+            (self.nparameters2fit + threads_per_block[2] - 1) // threads_per_block[2],
+            )
+        return func[blocks_per_grid, threads_per_block]
+
+
+
+    # --- Gradient ---
+
+    def gradient(self) :
+        self.estimator.loss(self.raw_data, self.model_data, weights=self.weights, out=self.loss_data, ignore=self.converged)
+        loss2gradient_function = self.gpu_loss2gradient if self.cuda else self.cpu_loss2gradient
+        loss2gradient_function(self.jacobian_data, self.loss_data, self.gradient_data, self.converged)
+
+    @prop(cache=True)
+    def cpu_loss2gradient(self) :
+        @nb.njit(parallel=True)
+        def func(jacobian, loss, gradient, converged) :
+            nmodels, npoints, nparams = jacobian.shape
+            for model in nb.prange(nmodels) :
+                if converged[model] : continue
+                for param in range(nparams) :
+                    s = 0.0
+                    for point in range(npoints) :
+                        s += jacobian[model, point, param] * loss[model, point]
+                    gradient[model, param, 0] = s
+        return func
+
+    @prop(cache=True)
+    def gpu_loss2gradient(self) :
+        @nb.cuda.jit()
+        def func(jacobian, loss, gradient, converged) :
+            nmodels, npoints, nparams = jacobian.shape
+            model, param = nb.cuda.grid(2)
+            if model < nmodels and not converged[model] and param < nparams :
+                s = 0.0
+                for point in range(npoints):
+                    s += jacobian[model, point, param] * loss[model, point]
+                gradient[model, param, 0] = s
+        threads_per_block = 16, 16
+        blocks_per_grid = (
+                (self.nmodels + threads_per_block[0] - 1) // threads_per_block[0],
+                (self.nparameters2fit + threads_per_block[1] - 1) // threads_per_block[1],
+                )
+        return func[blocks_per_grid, threads_per_block]
+
+
+
+    # --- Hessian ---
+
+    def hessian(self) :
+        self.estimator.fisher(self.raw_data, self.model_data, weights=self.weights, out=self.fisher_data, ignore=self.converged)
+        fisher2hessian_function = self.gpu_fisher2hessian if self.cuda else self.cpu_fisher2hessian
+        fisher2hessian_function(self.jacobian_data, self.fisher_data, self.hessian_data, self.converged)
+
+    @prop(cache=True)
+    def cpu_fisher2hessian(self) :
+        @nb.njit(parallel=True)
+        def func(jacobian, fisher, hessian, converged) :
+            nmodels, npoints, nparams = jacobian.shape
+            for model in nb.prange(nmodels) :
+                if converged[model] : continue
+                for param in range(nparams) :
+                    for paramT in range(nparams) :
+                        s = 0.0
+                        for point in range(npoints) :
+                            s += jacobian[model, point, param] * jacobian[model, point, paramT] * fisher[model, point]
+                        hessian[model, param, paramT] = s
+        return func
+
+    @prop(cache=True)
+    def gpu_fisher2hessian(self) :
+        @nb.cuda.jit()
+        def func(jacobian, fisher, hessian, converged) :
+            nmodels, npoints, nparams = jacobian.shape
+            model, param, paramT = nb.cuda.grid(3)
+            if model < nmodels and not converged[model] and param < nparams and paramT < nparams :
+                s = 0.0
+                for point in range(npoints):
+                    s += jacobian[model, point, param] * jacobian[model, point, paramT] * fisher[model, point]
+                hessian[model, param, paramT] = s
+        threads_per_block = 8, 8, 8
+        blocks_per_grid = (
+                (self.nmodels + threads_per_block[0] - 1) // threads_per_block[0],
+                (self.nparameters2fit + threads_per_block[1] - 1) // threads_per_block[1],
+                (self.nparameters2fit + threads_per_block[2] - 1) // threads_per_block[2],
+                )
+        return func[blocks_per_grid, threads_per_block]
+
+
+
+    # --- Convergence ---
+
+    def convergence(self) :
+        convergence_function = self.gpu_convergence if self.cuda else self.cpu_convergence
+        convergence_function(self.ftol, self.chi2_cache, self.chi2_data, self.gtol, self.gradient, self.xtol, self.delta, self.converged)
+
+    @prop(cache=True)
+    def cpu_convergence(self) :
+        @nb.njit(parallel=True)
+        def func(ftol, old_chi2, new_chi2, gtol, gradient, xtol, delta, converged) :
+            nmodels, nparams, _ = gradient.shape
+            for model in nb.prange(nmodels) :
+                if converged[model] : continue
+                # ftol
+                if np.abs(new_chi2[model] - old_chi2[model]) <= ftol :
+                    converged[model] = True
+                    continue
+                # gtol
+                maxi = 0.0
+                for param in range(nparams) :
+                    g = abs(gradient[model, param, 0])
+                    if maxi < g : maxi = g
+                if maxi <= gtol :
+                    converged[model] = True
+                    continue
+                # xtol
+                sum = 0.0
+                for param in range(nparams) :
+                    sum += delta[model, param, 0]**2
+                if sum <= xtol :
+                    converged[model] = True
+                    continue
+        return func
+
+    @prop(cache=True)
+    def gpu_convergence(self) :
+        @nb.cuda.jit()
+        def func(ftol, old_chi2, new_chi2, gtol, gradient, xtol, delta, converged) :
+            model = nb.cuda.grid(1)  # 1D grid of threads
+            nmodels, nparams, _ = gradient.shape
+            if model < nmodels and not converged[model] :
+                # ftol
+                if np.abs(new_chi2[model] - old_chi2[model]) <= ftol :
+                    converged[model] = True
+                    return
+                # gtol
+                maxi = 0.0
+                for param in range(nparams) :
+                    g = abs(gradient[model, param, 0])
+                    if maxi < g : maxi = g
+                if maxi <= gtol :
+                    converged[model] = True
+                    return
+                # xtol
+                sum = 0.0
+                for param in range(nparams) :
+                    sum += delta[model, param, 0]**2
+                if sum <= xtol :
+                    converged[model] = True
+                    return
+        threads_per_block = 128
+        blocks_per_grid = (self.nmodels + threads_per_block - 1) // threads_per_block
+        return func[blocks_per_grid, threads_per_block]
 
 
 
---
+++ src/funclp/modules/Fit_LP/_functions/LM.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Date          : 2026-01-01
+# Author        : Lancelot PINCET
+# GitHub        : https://github.com/LancelotPincet
+# Library       : funcLP
+
+
+
+# %% Libraries
+from funclp import Fit
+
+
+
+# %% Function
+
+class LM(Fit) :
+
+    # Attributs
+    damping_init = 1e-2 #lm damping
+    damping_increase = 7.0 # Factor to apply when damping
+    damping_decrease = 0.4 # Factor to apply when damping
+    damping_min, damping_max = 1e-6, 1e6 #damping limit values
+    max_retries = 10 # Maximum of step retries for a given hessian
+
+    # Functions
+
+    def fit_init(self) :
+        self.subconverged = self.xp.zeros_like(self.converge)
+        self.damping_data = self.xp.full(shape=self.nmodels, dtype=self.dtype, fill_value=self.damping_init)
+        self.hessian_diag = self.xp.empty(shape=(self.nmodels, self.nparameters2fit), dtype=self.dtype)
+        
+    def fit_optimize(self)
+        self.subconverged[:] = self.converged
+        self.hessian_Hdiag()
+
+        # Looping several time on a given hessian with different dampings
+        for _ in range(self.max_retries) :
+
+            # Apply damping : H = Hessian + damping * Identity
+            self.damping()
+
+            # Solve function
+            delta = self.xp.linalg.solve(self.hessian_data, -self.gradient_data) # TODO
+
+            # Apply steps
+            # TODO
+
+            self.improved
+
+            # evaluate chi2 after step
+            self.model()
+            self.chi2()
+
+
+
+
+    # --- Hessian diagonal ---
+
+    def hessian_Hdiag(self) :
+        Hdiag_function = self.gpu_Hdiag if self.cuda else self.cpu_Hdiag
+        Hdiag_function(self.hessian, self.hessian_diag, self.converged)
+
+    @prop(cache=True)
+    def cpu_Hdiag(self) :
+        @nb.njit(parallel=True)
+        def func(hessian, hessian_diag, converged) :
+            nmodels, nparams = hessian_diag.shape
+            for model in nb.prange(nmodels) :
+                if converged[model] : continue
+                for param in range(nparams) :
+                    hessian_diag[model, param] = hessian[model, param, param]
+        return func
+
+    @prop(cache=True)
+    def gpu_Hdiag(self) :
+        @nb.cuda.jit()
+        def func(hessian, hessian_diag, converged) :
+            nmodels, nparams = hessian_diag.shape
+            model, param = nb.cuda.grid(2)
+            if model < nmodels and not converged[model] and param < nparams :
+                hessian_diag[model, param] = hessian[model, param, param]
+        threads_per_block = 16, 16
+        blocks_per_grid = (
+                (self.nmodels + threads_per_block[0] - 1) // threads_per_block[0],
+                (self.nparameters2fit + threads_per_block[1] - 1) // threads_per_block[1],
+                )
+        return func[blocks_per_grid, threads_per_block]
+
+
+
+    # --- Damping ---
+
+    def damping(self) :
+        damping_function = self.gpu_damping if self.cuda else self.cpu_damping
+        damping_function(self.hessian, self.hessian_diag, self.damping_data, self.subconverged)
+
+    @prop(cache=True)
+    def cpu_damping(self) :
+        @nb.njit(parallel=True)
+        def func(hessian, hessian_diag, damping, converged) :
+            nmodels, nparam = hessian_diag.shape
+            for model in nb.prange(nmodels) :
+                if converged[model] : continue
+                for param in range(nparams) :
+                    hessian[model, param, param] = hessian_diag[model, param] + damping[model]
+        return func
+
+    @prop(cache=True)
+    def gpu_damping(self) :
+        @nb.cuda.jit()
+        def func(hessian, hessian_diag, damping, converged) :
+            nmodels, nparam = hessian_diag.shape
+            model, param = nb.cuda.grid(2)
+            if model < nmodels and not converged[model] and param < nparams :
+                hessian[model, param, param] = hessian_diag[model, param] + damping[model]
+        threads_per_block = 16, 16
+        blocks_per_grid = (
+                (self.nmodels + threads_per_block[0] - 1) // threads_per_block[0],
+                (self.nparameters2fit + threads_per_block[1] - 1) // threads_per_block[1],
+                )
+        return func[blocks_per_grid, threads_per_block]
+
